d9883154d0d1ad70afdbbd55166d2090d8f744a5
Enes SVG <enessvg4@gmail.com>
2024-06-25 12:33:52 +0300

    11-ternary-operator
    ternaty operator sayesinde if else olayını kısaltmış olduk.

---
+++ 11-ternary-operator/index.js
@@ -0,0 +1,8 @@
+const username = "Enes";
+const password = "1234567";
+
+
+//?True döndürdü          true döndürürse          false döndürürse 
+//?                        çalışacak yer            çalışacak yer         
+password.length > 6 ? console.log("çalıştı") : console.log("çalışmadı");
+username.length > 4 ? console.log("username doğru") : console.log("username yanlış");
