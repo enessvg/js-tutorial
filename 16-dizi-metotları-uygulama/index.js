a0c00a2116dac9998682f71b847109a8fcb96c4d
Enes SVG <enessvg4@gmail.com>
2024-06-26 15:05:14 +0300

    16-dizi-metotları-uygulama

---
+++ 16-dizi-metotları-uygulama/index.js
@@ -0,0 +1,12 @@
+let customers = ["enes", "taha", "efe"];
+
+let customers1 = ["enes", ["asus", "monster", "dell", [10,20,30] ]]
+
+// console.log(`Ürün: ${customers1[1][0]} fiyatı: ₺${customers1[1][3][0]}`);
+
+//dizinin ilk elemanı
+console.log(customers[0]);
+
+//dizinin son elemanı
+console.log(customers[customers.length - 1]);
+console.log(customers.length - 1);
